dcbffe4988c543fca0522a81fc1c2564f86718b5
houseofsam <samxsaid@gmail.com>
2021-10-28 00:40:20 +0000

    looping range array challenge

--- arrays.js
+++ arrays.js
@@ -35,3 +35,22 @@ let totals = bills.map(function(totals) {
 console.log(totals);
 
 //----------------------
+let range = function(start, end, step) {
+  let array = [start];
+
+  if (start === undefined || end === undefined || step === undefined || start > end || step <= 0){
+    return [];
+  }else {
+    for (let i = 0; array[i] + step <= end; i++) {
+      start += step;
+      array.push(start);
+    }
+
+    return array;
+  }
+}
+
+console.log(range(2,10,2));
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
