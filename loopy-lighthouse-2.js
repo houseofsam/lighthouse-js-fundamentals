fa79a9e2ccfcd8422ec91063d561102d62d6c8e9
houseofsam <samxsaid@gmail.com>
2021-11-23 05:39:39 +0000

    W1D1 Loopy Lighthouse Refactor

--- loopy-lighthouse-2.js
+++ loopy-lighthouse-2.js
@@ -1,15 +1,33 @@
-let loopyLighthouse = function (range, multiples, words) {
+// let loopyLighthouse = function (range, multiples, words) {
+//   for (let i = range[0]; i <= range[1]; i++) {
+//     if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+//       console.log(`${words[0]}${words[1]}`);
+//     } else if (i % multiples[0] === 0) {
+//       console.log(`${words[0]}`);
+//     } else if (i % multiples[1] === 0) {
+//       console.log(`${words[1]}`);
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
+
+
+
+let loopyLighthouse = function(range, multiples, words) {
   for (let i = range[0]; i <= range[1]; i++) {
-    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
-      console.log(`${words[0]}${words[1]}`);
-    } else if (i % multiples[0] === 0) {
-      console.log(`${words[0]}`);
-    } else if (i % multiples[1] === 0) {
-      console.log(`${words[1]}`);
-    } else {
-      console.log(i);
-    }
+    let output = '';
+
+    // 0 = false. Therefore, zero remainder (aka divisible) = false
+    !(i % multiples[0]) ? output += words[0] : ''; //if divisible by 1st number, add it to output
+    !(i % multiples[1]) ? output += words[1] : ''; //if divisible by 2nd number, add it to output (as well)
+
+    // Is there a true output? If so print it out, else just print the number.
+    console.log(output ? output : i);
   }
-}
+};
+
 
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
